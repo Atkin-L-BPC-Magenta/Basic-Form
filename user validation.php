adec43d9d0f7f4c32dabd82851196205effb2fb0
Louis Atkin <32098761+Atkin-L-BPC-Magenta@users.noreply.github.com>
2017-11-28 13:42:46 +0000

    Add files via upload

---
+++ user validation.php
@@ -0,0 +1,11 @@
+<?php
+
+$uname = $_POST['username'];
+$pass = $_POST['password'];
+
+if [$uname == "Trevor" && $pass == "pass"]
+	echo "Content after login has been successful"
+else
+	echo "invalid";
+
+?>
