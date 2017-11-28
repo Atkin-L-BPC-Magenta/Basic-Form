491a4c7a8482f9e0c702e0e74cdcd3a1ac38efc3
Louis Atkin <32098761+Atkin-L-BPC-Magenta@users.noreply.github.com>
2017-11-28 14:16:14 +0000

    Update login.php

--- login.php
+++ login.php
@@ -3,7 +3,7 @@
 $uname = $_POST['username'];
 $pass = $_POST['password'];
 
-if [$uname == "Trevor" && $pass == "pass"]
+if ($uname == "Trevor" && $pass == "pass")
 	echo "Content after login has been successful";
 else
 	echo "invalid";
